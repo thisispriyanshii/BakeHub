62b0ecb6a3ee7e2af66a293974abe2a0473e31dd
Gantyala Sreehitha <sree@Gantyalas-MacBook-Air.local>
2026-06-22 16:40:01 +0530

    aws

--- src/main/java/com/bakehub/my/config/AwsConfig.java
+++ src/main/java/com/bakehub/my/config/AwsConfig.java
@@ -3,7 +3,10 @@ package com.bakehub.my.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -13,11 +16,26 @@ public class AwsConfig {
     @Value("${aws.region}")
     private String awsRegion;
 
+    @Value("${aws.access-key-id:}")
+    private String awsAccessKeyId;
+
+    @Value("${aws.secret-access-key:}")
+    private String awsSecretAccessKey;
+
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(awsRegion))
-                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .credentialsProvider(buildCredentialsProvider())
                 .build();
+    }
+
+    private AwsCredentialsProvider buildCredentialsProvider() {
+        if (awsAccessKeyId != null && !awsAccessKeyId.isBlank()
+                && awsSecretAccessKey != null && !awsSecretAccessKey.isBlank()) {
+            return StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(awsAccessKeyId, awsSecretAccessKey));
+        }
+        return DefaultCredentialsProvider.create();
     }
 }
